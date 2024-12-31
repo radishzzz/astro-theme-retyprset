10d719a9402ff63898ed2a070a0cbdb116bd5176
“radishzzz” <shuyinsen@gmail.com>
2024-12-31 19:02:45 +0000

    version 0.1.5 / Unocss rules

--- uno.config.ts
+++ uno.config.ts
@@ -28,29 +28,32 @@ export default defineConfig({
   },
   shortcuts: {
     'post-title': 'text-5 font-bold lh-7.5 m-0',
+    'se': 'text-5 font-bold lh-7.5 m-0',
   },
   transformers: [
     transformerDirectives(), 
     transformerVariantGroup()
   ],
   rules: [
-    [
-      /^grid-\[([\w\d\s_-]+)\]-\[([\w\d\s_-]+)\]$/,
-      
-      ([, cols, rows]) => ({
-        display: 'grid',
-        'grid-template-columns': cols.replace(/_/g, ' '),
-        'grid-template-rows': rows.replace(/_/g, ' ')
-      })
-    ],
-    [
-      /^se-(\d+)-(\d+)-(\d+)-(\d+)$/,
-      ([, colStart, colEnd, rowStart, rowEnd]) => ({
-        'grid-column-start': colStart,
-        'grid-column-end': colEnd,
-        'grid-row-start': rowStart,
-        'grid-row-end': rowEnd,
-      }),
-    ],
+    // grid-x-y
+    [/^grid-(\d+|\[.+?\])-(\d+|\[.+?\])$/, ([, cols, rows]) => {
+        const isColsBracket = cols.startsWith('[') && cols.endsWith(']');
+        const isRowsBracket = rows.startsWith('[') && rows.endsWith(']');
+        return {
+          display: 'grid',
+          'grid-template-columns': isColsBracket
+            ? cols.slice(1, -1).replace(/_/g, ' ')
+            : `repeat(${cols}, 1fr)`,
+          'grid-template-rows': isRowsBracket
+            ? rows.slice(1, -1).replace(/_/g, ' ')
+            : `repeat(${rows}, 1fr)`,
+    }}],
+    // se-1-2-3-4
+    [/^se-(\d+)-(\d+)-(\d+)-(\d+)$/, ([, startX, endX, startY, endY]) => ({
+      'grid-column-start': startX,
+      'grid-column-end': endX,
+      'grid-row-start': startY,
+      'grid-row-end': endY,
+    })],
   ],
 })
